efc47de857ec4a9391185308d25119d4bd734271
chandni dalal <chandnidalal2@gmail.com>
2024-03-02 09:04:12 +0530

    2 commit  - Added Routing Login/Sign up

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Signup  from '../features/auth/component/Signup'
+const SignupPage = () => {
+  return (
+    <div>
+      <Signup/>
+    </div>
+  )
+}
+
+export default SignupPage
